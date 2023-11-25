7d8e7c42bf577253d77b5dd8c5eb1db7d9fd3536
Forhad96 <forhadairdrop@gmail.com>
2023-11-26 01:29:59 +0600

    created api for get users data

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -2,10 +2,29 @@ const users = require("../../models/users");
 
 const router = require("express").Router();
 
-router.post("/users", async (req, res) => {
-  const user = req.body;
-  const result = await users.insertMany(user);
-  res.send(result);
+// get method
+router.get('/users',async(req,res,next)=>{
+  try {
+    const result = await users.find()
+    res.send(result)
+  } catch (error) {
+    next(error)
+
+    
+  }
+})
+// post method
+router.post("/users", async (req, res,next) => {
+try {
+    const user = req.body;
+    const result = await users.insertMany(user);
+    res.send(result);
+} catch (error) {
+  next(error)
+  
+}
 });
+
+
 
 module.exports = router;
