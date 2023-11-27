21803669ede86cf75d8bf128db8f256a9be29744
Forhad96 <forhadairdrop@gmail.com>
2023-11-28 03:06:42 +0600

    create api for display all reported products

--- src/routes/report/index.js
+++ src/routes/report/index.js
@@ -1,20 +1,41 @@
+const products = require("../../models/Products");
 const Report = require("../../models/Report");
 
 const router = require("express").Router();
 
+// get all reported product
+router.get("/allReportedProducts", async (req, res, next) => {
+  try {
+    //get all report
+    const reports = await Report.find();
+
+    // get all reported id
+    const reportedId = reports.map((report) => report.productId);
+    // get all reported product
+
+    // console.log(reportedId);
+    // return
+
+    const reportedProducts = await products.find({
+      _id: { $in:  reportedId },
+    });
+    console.log(reportedProducts);
+    return;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // post method
-router.post('/report',async(req,res,next)=>{
-    try {
-        const newReport = req.body
-        console.log(newReport);
-        const result = await Report.insertMany(newReport)
-        res.send({success:true})
-    } catch (error) {
-        next(error)
-        
-    }
-})
-
-
+router.post("/report", async (req, res, next) => {
+  try {
+    const newReport = req.body;
+    console.log(newReport);
+    const result = await Report.insertMany(newReport);
+    res.send({ success: true });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
